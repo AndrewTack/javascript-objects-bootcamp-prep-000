8fb7b400abc071dc9662af735b49033aa6cf35db
Andrew Tack <andrew.tack.recapllc@gmail.com>
2019-05-29 14:19:05 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -6,5 +6,11 @@ var playlist = {
 }
 
 function updatePlaylist (playlist, artist, title) {
+  playlist[artist] = title
+  
+  return playlist
+}
+
+function removeFromPlaylist (playlist, artist) {
   
 }
