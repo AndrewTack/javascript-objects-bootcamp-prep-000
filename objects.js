9dcb8bb933b48b64a0d0e93461dd70cacea375b1
Andrew Tack <andrew.tack.recapllc@gmail.com>
2019-05-29 14:21:14 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,8 +1,8 @@
 var playlist = {
-  beatles : blackbird,
-  queen : bohemian, 
-  billy : pianoman,
-  elton : rocketman,
+  beatles : "blackbird",
+  queen : "bohemian", 
+  billy : "pianoman",
+  elton : "rocketman",
 }
 
 function updatePlaylist (playlist, artist, title) {
